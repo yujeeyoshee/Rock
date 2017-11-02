b90162c405ad3c9a74d414208a484808f455460f
Yuji <yujeeyoshee@gmail.com>
2017-11-02 10:13:24 -0700

    Add listener to setOnCheckedChangeListener of the switch, use companion object instead of static final variable for LOG_TAG

--- app/src/main/java/com/basicpencil/screenshot/ui/MainActivity.kt
+++ app/src/main/java/com/basicpencil/screenshot/ui/MainActivity.kt
@@ -2,12 +2,28 @@ package com.basicpencil.screenshot.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
 import com.basicpencil.screenshot.R
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        val LOG_TAG = MainActivity::class.qualifiedName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val enableNotificationSwitch = findViewById<Switch>(R.id.enable_notification_switch)
+        enableNotificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Log.w(LOG_TAG, "on")
+            } else {
+                Log.w(LOG_TAG, "off")
+            }
+
+        }
     }
 }
