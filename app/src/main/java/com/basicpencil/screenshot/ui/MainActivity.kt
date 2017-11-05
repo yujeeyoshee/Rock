0762b8d7c9880b2d0771ed49784e284c29538e61
Yuji <yujeeyoshee@gmail.com>
2017-11-05 09:12:20 -0800

    Show local notification when toggling the switch

--- app/src/main/java/com/basicpencil/screenshot/ui/MainActivity.kt
+++ app/src/main/java/com/basicpencil/screenshot/ui/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.basicpencil.screenshot.ui
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,17 +13,27 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         val LOG_TAG = MainActivity::class.qualifiedName
+        val NOTIFY_ID = 54321
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val notifactionManager = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+
         val enableNotificationSwitch = findViewById<Switch>(R.id.enable_notification_switch)
         enableNotificationSwitch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                val notificationPayload = Notification.Builder(this)
+                        .setContentTitle("Hello")
+                        .setContentText("world")
+                        .setSmallIcon(R.drawable.ic_stat_onesignal_default)
+                        .build()
+                notifactionManager?.notify(NOTIFY_ID, notificationPayload);
                 Log.w(LOG_TAG, "on")
             } else {
+                notifactionManager?.cancel(NOTIFY_ID);
                 Log.w(LOG_TAG, "off")
             }
 
