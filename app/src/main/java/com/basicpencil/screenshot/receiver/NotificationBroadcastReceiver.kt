098ae4a806a5f78d9864514fb3212b51cd7abadd
Yuji <yujeeyoshee@gmail.com>
2017-11-25 16:15:53 -0800

    Transparent activity that'll be used for capturing

--- app/src/main/java/com/basicpencil/screenshot/receiver/NotificationBroadcastReceiver.kt
+++ app/src/main/java/com/basicpencil/screenshot/receiver/NotificationBroadcastReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import com.basicpencil.screenshot.ui.MainActivity
+import com.basicpencil.screenshot.ui.ScreenshotActivity
 
 class NotificationBroadcastReceiver : BroadcastReceiver() {
     companion object {
@@ -18,8 +19,16 @@ class NotificationBroadcastReceiver : BroadcastReceiver() {
         when (intent.action) {
             MainActivity.ACTION_NOTIFICATION_CLICKED -> {
                 Log.w(LOG_TAG, intent.action)
-                val intent = Intent(context, NotificationService::class.java)
-                context.startService(intent)
+
+                // Service
+//                val intent = Intent(context, NotificationService::class.java)
+//                context.startService(intent)
+
+                // Activity
+                val intent = Intent(context, ScreenshotActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // crashes without it
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION); // no transition
+                context.startActivity(intent)
             }
 
             MainActivity.ACTION_NOTIFICATION_CANCELED -> {
---
+++ app/src/main/java/com/basicpencil/screenshot/ui/ScreenshotActivity.kt
@@ -0,0 +1,13 @@
+package com.basicpencil.screenshot.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.basicpencil.screenshot.R
+
+class ScreenshotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screenshot)
+    }
+}
